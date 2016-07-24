bb30f6f52be22a771c5aa40670d75efa7f42fcc8
Claus Still <claus.still@gmail.com>
2016-07-24 12:12:55 +0300

    chmod 666 before echoing to fstab

--- init_stb/init_home_directory.sh
+++ init_stb/init_home_directory.sh
@@ -7,8 +7,9 @@ sudo umount /media/pi/home
 sudo rm -rf /media/pi/home
 sudo mv /home /old_home
 sudo mkdir home
-sudo chmod 664 /etc/fstab
+sudo chmod 666 /etc/fstab
 sudo echo "/dev/mmcblk0p8 /home ext4 defaults,noatime,nodiratime 0 0" >> /etc/fstab
+sudo chmod 444 /etc/fstab
 sudo mount /home
 sudo rm -fr /old_home
 
